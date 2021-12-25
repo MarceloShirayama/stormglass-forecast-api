0f6fcca2cdd26219c87e6a16ad06925a0a944058
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-12-25 09:39:49 -0300

    ensure that the forecast controller is able to return the forecast for the beaches

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -3,6 +3,7 @@ import nock from 'nock'
 import { SetupServer } from '@src/server'
 import { Beach, BeachPosition } from '@src/models/beach'
 import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
+import apiForecastResponse1BeachFixture from '@test/fixtures/api_forecast_response_1_beach.json'
 
 let response: supertest.SuperTest<supertest.Test>
 let server: SetupServer
@@ -27,69 +28,9 @@ describe('Beach forecast functional tests', () => {
     await server.close()
   })
 
-  it('Should return a forecast with just a few times', async () => {
-    const expectedResponse = [
-      {
-        time: '2020-04-26T00:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 1,
-            swellDirection: 64.26,
-            swellHeight: 0.15,
-            swellPeriod: 3.89,
-            time: '2020-04-26T00:00:00+00:00',
-            waveDirection: 231.38,
-            waveHeight: 0.47,
-            windDirection: 299.45,
-            windSpeed: 100
-          }
-        ]
-      },
-      {
-        time: '2020-04-26T01:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 1,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2020-04-26T01:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-            windSpeed: 100
-          }
-        ]
-      },
-      {
-        time: '2020-04-26T02:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 1,
-            swellDirection: 182.56,
-            swellHeight: 0.28,
-            swellPeriod: 3.44,
-            time: '2020-04-26T02:00:00+00:00',
-            waveDirection: 232.86,
-            waveHeight: 0.46,
-            windDirection: 321.5,
-            windSpeed: 100
-          }
-        ]
-      }
-    ]
+  it('Should return the forecast for the beaches', async () => {
+    const dataSendInRequest = stormGlassWeather3HoursFixture
+    const expectedResponse = apiForecastResponse1BeachFixture
 
     nock('https://api.stormglass.io:443', {
       encodedQueryParams: true,
@@ -105,7 +46,7 @@ describe('Beach forecast functional tests', () => {
         params: /(.*)/,
         source: 'noaa'
       })
-      .reply(200, stormGlassWeather3HoursFixture)
+      .reply(200, dataSendInRequest)
 
     const { body, status } = await response.get('/forecast')
 
