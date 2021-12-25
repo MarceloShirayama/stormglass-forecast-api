e4940af1d7ee79c6aa73a8d433bd169c64842117
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-12-25 10:26:44 -0300

    start server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World')
+import config from 'config'
+import { SetupServer } from './server'
+// #endregion
+;(async (): Promise<void> => {
+  const server = new SetupServer(config.get('App.port'))
+  await server.init()
+  server.start()
+})().catch(console.error)
