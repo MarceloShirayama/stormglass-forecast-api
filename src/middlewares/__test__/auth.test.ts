710c94a1b1d16fa92d66719f3f54376b6a08940d
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-12-27 22:07:29 -0300

    ensure AuthMiddleware return unauthorized if there is a problem on the token verification

--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -16,4 +16,48 @@ describe('AuthMiddleware', () => {
 
     expect(nextFake).toHaveBeenCalled()
   })
+
+  it('Should return unauthorized if there is a problem on the token verification', () => {
+    const reqFake = {
+      headers: {
+        'x-access-token': 'invalid-token'
+      }
+    }
+    const sendMock = jest.fn()
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock
+      }))
+    }
+    const nextFake = jest.fn()
+
+    authMiddleware(reqFake, resFake as object, nextFake)
+
+    expect(resFake.status).toHaveBeenCalledWith(401)
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 401,
+      error: 'jwt malformed or no provided'
+    })
+  })
+
+  it('Should return unauthorized if token not provided', () => {
+    const reqFake = {
+      headers: {}
+    }
+    const sendMock = jest.fn()
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock
+      }))
+    }
+    const nextFake = jest.fn()
+
+    authMiddleware(reqFake, resFake as object, nextFake)
+
+    expect(resFake.status).toHaveBeenCalledWith(401)
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 401,
+      error: 'jwt malformed or no provided'
+    })
+  })
 })
